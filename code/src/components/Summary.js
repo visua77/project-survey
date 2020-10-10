6f80f087924a3a40047778f8a154a291729f576a
Niclas Binnquist <niclasbinnquist@h-4-121-182.A148.priv.bahnhof.se>
2020-10-10 20:14:00 +0200

    added color under summary

--- code/src/components/Summary.js
+++ code/src/components/Summary.js
@@ -11,7 +11,7 @@ const Summary = ({happiness, feeling, reason, color})=> {
               </span>}</p>
             <p>You feel: {feeling}</p>
             <p>Becuase: {reason}</p>
-            <p>And your current color is: {color}</p>
+            <p>And your current color is: <span style={{background: `linear-gradient(to right, ${color}, #bdfff3)`}}>My color</span></p>
         </div>
     )
 }
